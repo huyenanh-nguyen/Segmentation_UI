561210591119335d867094a59fa60d85e69e7b0c
huyenanh-nguyen <ng_huyenanh@mailbox.org>
2026-09-11 10:20:03 +0200

    ok clean soon

--- Segmentation/trainingtest.py
+++ Segmentation/trainingtest.py
@@ -41,6 +41,23 @@ def normalizing_image(img, low, high):
     scaled_data = np.clip((img - data_min) / ( data_max - data_min + 1e-8), 0, 1)
     return scaled_data
 
+# ================================================================================================================================================================================================================================================
+# Torch on GPU instead of CPU
+# ================================================================================================================================================================================================================================================
+
+os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "0" # IMPORTANT: Disable MPS CPU fallback
+
+# For Apple M2 pro Chip
+if not torch.backends.mps.is_available():
+    raise RuntimeError(
+        "MPS is not available! "
+        "Training stopped so the CPU will not be used."
+    )
+
+device = torch.device("mps")
+
+print(f"Using device: {device}")
+
 
 # # ================================================================================================================================================================================================================================================
 # # Data
@@ -187,3 +204,236 @@ def converting_roi_to_tiff(folderpath, outputfolder):
             print(f"  Error processing {roi_path.name}: {e}")
 
     return None
+
+
+# ================================================================================================================================================================================================================================================
+# Creating UNET
+# ================================================================================================================================================================================================================================================
+
+"""
+What is an UNET? (Source: Wikipedia, https://en.wikipedia.org/wiki/U-Net)
+
+The U-Net architecture stems from the so-called "fully convolutional network". 
+The network consists of a contracting path and an expansive path, which gives it the u-shaped architecture. 
+The contracting path is a typical convolutional network that consists of repeated application of convolutions, each followed by a rectified linear unit (ReLU) and a max pooling operation. 
+During the contraction, the spatial information is reduced while feature information is increased. 
+The expansive pathway combines the feature and spatial information through a sequence of up-convolutions and concatenations with high-resolution features from the contracting path
+"""
+
+"""
+What is convolution? (Source: Wikipedia, https://en.wikipedia.org/wiki/Convolution)
+In mathematics (in particular, functional analysis), convolution is a mathematical operation on two functions f and g that produces a third function f∗g, 
+as the integral of the product of the two functions after one is reflected about the y-axis and shifted.
+The term convolution refers to both the resulting function and to the process of computing it.
+"""
+
+
+class SmallUNet(nn.Module):
+
+    def __init__(
+        self,
+        in_channels=5,
+        out_channels=2
+    ):
+
+        super().__init__()
+
+        # --------------------
+        # Encoder
+        # --------------------
+
+        self.enc1 = DoubleConv(
+            in_channels,
+            32
+        )
+
+        self.enc2 = DoubleConv(
+            32,
+            64
+        )
+
+        self.enc3 = DoubleConv(
+            64,
+            128
+        )
+
+        self.pool = nn.MaxPool2d(2)
+
+
+        # --------------------
+        # Bottleneck
+        # --------------------
+
+        self.bottleneck = DoubleConv(
+            128,
+            256
+        )
+
+
+        # --------------------
+        # Decoder
+        # --------------------
+
+        self.up3 = nn.ConvTranspose2d(
+            256,
+            128,
+            kernel_size=2,
+            stride=2
+        )
+
+        self.dec3 = DoubleConv(
+            256,
+            128
+        )
+
+
+        self.up2 = nn.ConvTranspose2d(
+            128,
+            64,
+            kernel_size=2,
+            stride=2
+        )
+
+        self.dec2 = DoubleConv(
+            128,
+            64
+        )
+
+
+        self.up1 = nn.ConvTranspose2d(
+            64,
+            32,
+            kernel_size=2,
+            stride=2
+        )
+
+        self.dec1 = DoubleConv(
+            64,
+            32
+        )
+
+
+        # --------------------
+        # Output
+        # --------------------
+
+        self.output = nn.Conv2d(
+            32,
+            out_channels,
+            kernel_size=1
+        )
+
+
+    def forward(self, x):
+
+        # Encoder
+
+        e1 = self.enc1(x)
+
+        e2 = self.enc2(
+            self.pool(e1)
+        )
+
+        e3 = self.enc3(
+            self.pool(e2)
+        )
+
+
+        # Bottleneck
+
+        b = self.bottleneck(
+            self.pool(e3)
+        )
+
+
+        # Decoder
+
+        d3 = self.up3(b)
+
+        d3 = torch.cat(
+            [d3, e3],
+            dim=1
+        )
+
+        d3 = self.dec3(d3)
+
+
+        d2 = self.up2(d3)
+
+        d2 = torch.cat(
+            [d2, e2],
+            dim=1
+        )
+
+        d2 = self.dec2(d2)
+
+
+        d1 = self.up1(d2)
+
+        d1 = torch.cat(
+            [d1, e1],
+            dim=1
+        )
+
+        d1 = self.dec1(d1)
+
+
+        return self.output(d1)
+
+
+# ============================================================
+# CREATE TEMPORAL WINDOW
+# ============================================================
+
+def get_temporal_window(
+    images,
+    t,
+    radius=2
+):
+
+    frames = []
+
+    num_frames = len(images)
+
+    for offset in range(-radius, radius + 1):
+
+        frame_index = t + offset
+
+        # Handle beginning/end of movie
+        frame_index = np.clip(
+            frame_index,
+            0,
+            num_frames - 1
+        )
+
+        frames.append(
+            images[frame_index]
+        )
+
+    # Shape:
+    # (5, height, width)
+
+    return np.stack(
+        frames,
+        axis=0
+    )
+
+
+"""
+Project arbeit:
+Investigation of methode for biological Image processing (was es für methoden gibt, wie algorithmische oder NN oder pretrained models)
+oder
+Application of cellular method
+
+Master thesis:
+Development pipline of Interleukin cells with method NN
+- test machen
+- annotated data genug davon
+- data augmentation
+- braucht man mehr data
+- verschiedene movies davon 10 frames, je mehr verschiedene daten desto besser
+- eine zelle rausnehmen und die verändern, modifzieren so rotieren, verschwimmen und die verfälschen -> 10 verschiedene transformationen
+- yolo11
+- wenn man eine datenart trainiert dann wird es overfitten
+trainingsdaten (20%) und testdaten (80%)
+"""
